fff9ad2f8add68ea8e695982db9a3e5c17fbf11a
Vaishnavi Rajendra Desai <vaishnavidesai957@gmail.com>
2025-05-05 09:35:27 +0530

    Update model_helper.py

--- model_helper.py
+++ model_helper.py
@@ -1,35 +1,38 @@
+import os
+import urllib.request
 import torch
 from torch import nn
 from torchvision import models, transforms
 from PIL import Image
 
+MODEL_PATH = "model/saved_model.pth"
+MODEL_URL = "https://drive.google.com/uc?id=1V0ghKNZjk_6Bjox7Qd9-N9Ncqft6zD_0"  # Direct download link
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs("model", exist_ok=True)
+        print("Downloading model from Google Drive...")
+        urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+        print("Model downloaded successfully.")
+
 trained_model = None
 class_names = ['Front Breakage', 'Front Crushed', 'Front Normal', 'Rear Breakage', 'Rear Crushed', 'Rear Normal']
 
-
-# Load the pre-trained ResNet model
 class CarClassifierResNet(nn.Module):
     def __init__(self, num_classes=6):
         super().__init__()
         self.model = models.resnet50(weights='DEFAULT')
-        # Freeze all layers except the final fully connected layer
         for param in self.model.parameters():
             param.requires_grad = False
-
-        # Unfreeze layer4 and fc layers
         for param in self.model.layer4.parameters():
             param.requires_grad = True
-
-            # Replace the final fully connected layer
         self.model.fc = nn.Sequential(
             nn.Dropout(0.2),
             nn.Linear(self.model.fc.in_features, num_classes)
         )
 
     def forward(self, x):
-        x = self.model(x)
-        return x
-
+        return self.model(x)
 
 def predict(image_path):
     image = Image.open(image_path).convert("RGB")
@@ -43,10 +46,9 @@ def predict(image_path):
     global trained_model
 
     if trained_model is None:
+        download_model()  # Automatically download if not present
         trained_model = CarClassifierResNet()
-        #trained_model.load_state_dict(torch.load("model\saved_model.pth"))
-        trained_model.load_state_dict(torch.load("model/saved_model.pth", map_location=torch.device('cpu')))
-
+        trained_model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
         trained_model.eval()
 
     with torch.no_grad():
