1dfd46190153bf54c5f3cdadd8babc178cd2bba9
Vaishnavi Rajendra Desai <vaishnavidesai957@gmail.com>
2025-05-05 09:56:49 +0530

    Update model_helper.py

--- model_helper.py
+++ model_helper.py
@@ -1,61 +1,53 @@
-import os
 import torch
 from torch import nn
 from torchvision import models, transforms
 from PIL import Image
-import gdown
 
-# Google Drive file ID and local path
-MODEL_ID = "1V0ghKNZjk_6Bjox7Qd9-N9Ncqft6zD_0"
-MODEL_PATH = "model/saved_model.pth"
-
-# Class labels
+trained_model = None
 class_names = ['Front Breakage', 'Front Crushed', 'Front Normal', 'Rear Breakage', 'Rear Crushed', 'Rear Normal']
 
-# Define the model architecture
+
+# Load the pre-trained ResNet model
 class CarClassifierResNet(nn.Module):
     def __init__(self, num_classes=6):
         super().__init__()
         self.model = models.resnet50(weights='DEFAULT')
+        # Freeze all layers except the final fully connected layer
         for param in self.model.parameters():
             param.requires_grad = False
+
+        # Unfreeze layer4 and fc layers
         for param in self.model.layer4.parameters():
             param.requires_grad = True
+
+            # Replace the final fully connected layer
         self.model.fc = nn.Sequential(
             nn.Dropout(0.2),
             nn.Linear(self.model.fc.in_features, num_classes)
         )
 
     def forward(self, x):
-        return self.model(x)
+        x = self.model(x)
+        return x
 
-# Download model if not present
-def download_model():
-    if not os.path.exists(MODEL_PATH):
-        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
-        print("Downloading model from Google Drive...")
-        gdown.download(id=MODEL_ID, output=MODEL_PATH, quiet=False)
-
-# Predict function
-trained_model = None
 
 def predict(image_path):
+    image = Image.open(image_path).convert("RGB")
+    transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    image_tensor = transform(image).unsqueeze(0)
+
     global trained_model
 
     if trained_model is None:
-        download_model()
         trained_model = CarClassifierResNet()
-        trained_model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
-        trained_model.eval()
+        #trained_model.load_state_dict(torch.load("model\saved_model.pth"))
+        trained_model.load_state_dict(torch.load("model\saved_model.pth", map_location=torch.device('cpu')))
 
-    image = Image.open(image_path).convert("RGB")
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
-    image_tensor = transform(image).unsqueeze(0)
+        trained_model.eval()
 
     with torch.no_grad():
         output = trained_model(image_tensor)
